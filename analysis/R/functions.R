a17268c2e7332f1863b4e398a7e87fce280b1576
DylanMG <dylan.glaser1@ucalgary.ca>
2024-01-25 13:15:20 -0800

    add WSP (biological benchmark) HCR

--- analysis/R/functions.R
+++ analysis/R/functions.R
@@ -14,6 +14,8 @@ quantile_df <- function(x, probs = c(0.025, 0.25, 0.5, 0.75, 0.975)){ #where pro
   tibble(val = quantile(x, probs, na.rm = TRUE))
 }
 
+
+# Several HCRs----------------------------------------------------------------------------
 current_HCR <- function(R, OU){
   if(R <= 7.059){C <- ((R*(0.15/7.059))*R)*OU
   C <- ifelse(C<0, 0, C)
@@ -32,10 +34,24 @@ current_HCR <- function(R, OU){
 alt_HCR <- function(R, OU){
   if(R <= 6){C <- 0
   S <- R}
-  if(R > 6 & R <= 20){C <- (R-6)*OU
+  if(R > 6 & R <= 15){C <- (R-6)*OU
   S <- R-C}
-  if(R > 20){C <- (R*0.7)*OU
+  if(R > 15){C <- (R*0.6)*OU
   C <- ifelse(C>R, R-.0001, C) #leave 100 spawners if catch OU makes C>R
+  S <- R-C}
+
+  U <- C/R
+
+  return(c(S,C,U))
+}
+
+BB_HCR <- function(R, OU, Sgen, Smsy, Umsy){
+  if(R <= Sgen){C <- 0
+  S <- R} #might need to add protection here if forecast error makes R<0
+  if(R > Sgen & R < Smsy){ C <- (R*(Umsy/Sgen)*R)
+  S <- R-C}
+  if(R > Smsy){C <- R*Umsy
+  C <- ifelse(C>R, R-.0001, C)
   S <- R-C}
 
   U <- C/R
