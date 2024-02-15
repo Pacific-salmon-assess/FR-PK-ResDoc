542c2846814565c3aea3e33338550941f606fe9c
DylanMG <dylan.glaser1@ucalgary.ca>
2024-02-15 10:37:17 -0800

    squish bug and make catch sim work

--- analysis/R/inference-figs.R
+++ analysis/R/inference-figs.R
@@ -42,7 +42,7 @@ SR_pred <- as.data.frame(cbind(spw,t(apply(SR_pred,c(1),quantile,probs=c(0.025,0
   round(2)
 colnames(SR_pred) <- c("Spawn", "Rec_lwr","Rec_med","Rec_upr")
 
-#create spawner & esc df ---
+#create spawner & esc df --- #should map() these...
 spwn.quant <- apply(model.pars$S, 2, quantile, probs=c(0.025,0.25,0.5,0.75,0.975))
 spwn_df <- as.data.frame(cbind(data$year, t(spwn.quant)))
 colnames(spwn_df) <- c("year","lwr","mid_lwr","mid","mid_upr","upr")
@@ -55,6 +55,10 @@ R.quant <- apply(model.pars$R, 2, quantile, probs=c(0.025,0.25,0.5,0.75,0.975))
 R_df <- as.data.frame(cbind(data$year, t(R.quant)))
 colnames(R_df) <- c("year","lwr","mid_lwr","mid","mid_upr","upr")
 
+C.quant <- apply(model.pars$C, 2, quantile, probs=c(0.025,0.25,0.5,0.75,0.975))
+C_df <- as.data.frame(cbind(data$year, t(C.quant)))
+colnames(C_df) <- c("year","lwr","mid_lwr","mid","mid_upr","upr")
+
 kobe_df <- data.frame(S = spwn_df$mid,
                       S_LCI = spwn_df$lwr,
                       S_UCI = spwn_df$upr,
@@ -63,47 +67,13 @@ kobe_df <- data.frame(S = spwn_df$mid,
                       U_UCI = er_df$upr,
                       year = spwn_df$year) |>
   #index benchmarks table
-  mutate(U_Umsy = round(U/benchmarks[3,1] ,2),
+  mutate(U_Umsy = round(U/benchmarks[3,1] ,2), ## index these as names!
          U_Umsy_LCI = round(U_LCI/benchmarks[3,1] ,2),
          U_Umsy_UCI = round(U_UCI/benchmarks[3,1] ,2),
          S_Smsy = round(S/(benchmarks[1,1]*1.25), 2), #correct from 80%Smsy to Smsy
          S_Smsy_LCI = round(S_LCI/(benchmarks[1,1]*1.25), 2),
          S_Smsy_UCI = round(S_UCI/(benchmarks[1,1]*1.25), 2))
-if(FALSE){
-#wrangle fwd sim spawners
-fwd.sim.S <- fwd.states.summary |>
-  select(year, HCR, grep("S", colnames(fwd.states.summary))) |>
-  rename(lwr = S_lwr,
-         mid_lwr = S_mid_lwr,
-         mid = S,
-         mid_upr = S_mid_upr,
-         upr = S_upr) |>
-  mutate(year = as.numeric(year)) |>
-  bind_rows(spwn_df) |>
-  arrange(year)
 
-fwd.sim.U <- fwd.states.summary |>
-  select(year, sim, grep("U", colnames(fwd.states.summary))) |>
-  rename(lwr = U_lwr,
-         mid_lwr = U_mid_lwr,
-         mid = U,
-         mid_upr = U_mid_upr,
-         upr = U_upr) |>
-  mutate(year = as.numeric(year)) |>
-  bind_rows(er_df) |>
-  arrange(year)
-
-fwd.sim.R <- fwd.states.summary |>
-  select(year, sim, grep("R", colnames(fwd.states.summary))) |>
-  rename(lwr = R_lwr,
-         mid_lwr = R_mid_lwr,
-         mid = R,
-         mid_upr = R_mid_upr,
-         upr = R_upr) |>
-  mutate(year = as.numeric(year)) |>
-  bind_rows(R_df) |>
-  arrange(year)
-}
 rm(er.quant, rec.quant, spwn.quant, max_samples, percentiles, a,b, spw)
 
 # INFERENCE ------------------------------------------------------------------------------
@@ -245,7 +215,7 @@ ggplot(data = fwd.sim) +
 #            fill = "darkred", alpha = 0.02) +
   geom_hline(yintercept = benchmarks$median[2], color = "darkred") +
   annotate("text", x = d_start + 2, y = 2,
-           label = "italic(S[Gen])", parse = TRUE, color = "darkred") +
+           label = "italic(S[gen])", parse = TRUE, color = "darkred") +
   scale_x_continuous(breaks= pretty_breaks()) +
   #coord_cartesian(expand = FALSE) +
   labs(x = "return year", y = "escapement") +
@@ -312,7 +282,7 @@ ggplot(data = fwd.sim) +
   #          fill = "darkred", alpha = 0.02) +
   geom_hline(yintercept = benchmarks$median[2], color = "darkred") +
   annotate("text", x = d_start + 2, y = 4,
-           label = "italic(S[Gen])", parse = TRUE, color = "darkred") +
+           label = "italic(S[gen])", parse = TRUE, color = "darkred") +
   scale_x_continuous(breaks= pretty_breaks()) +
   #coord_cartesian(expand = FALSE) +
   labs(x = "return year", y = "recruits") +
@@ -327,7 +297,34 @@ ggplot(data = fwd.sim) +
 
 my.ggsave(here("figure/fwd-R.png"))
 
+#and fwd sim catch
+ggplot(data = fwd.sim) +
+  #draw the fwd.sim
+  geom_line(aes(x = year, y = C, color = HCR), lwd = 1, lty = 2) +
+  #geom_ribbon(aes(x = year, ymin = U_lwr, ymax = U_upr,  fill = HCR), alpha = 0.5) +
+  geom_ribbon(aes(x = year, ymin = C_mid_lwr, ymax = C_mid_upr, fill = HCR), alpha=0.2) +
+  #draw the exsiting data
+  geom_line(data = filter(C_df, year >=d_start, year <= d_end), aes(x = year, y = mid)) +
+  geom_ribbon(data = filter(C_df, year >=d_start, year <= d_end),
+              aes(x = year, ymin = lwr, ymax = upr),  fill = "darkgrey", alpha = 0.5) +
+  geom_ribbon(data = filter(C_df, year >=d_start, year <= d_end),
+              aes(x = year, ymin = mid_lwr, ymax = mid_upr), fill = "black", alpha=0.2) +
+  scale_x_continuous(breaks= pretty_breaks()) +
+  #coord_cartesian(expand = FALSE) +
+  labs(x = "return year", y = "catch (M)") +
+  scale_fill_viridis_d(name = "HCR",
+                       breaks = c("current", "alt", "WSP"),
+                       labels = c("current", "alternate", "WSP")) +
+  scale_color_viridis_d(name = "HCR",
+                        breaks = c("current", "alt", "WSP"),
+                        labels = c("current", "alternate", "WSP")) +
+  facet_grid(prod~.) +
+  theme(legend.position = "bottom")
+
+my.ggsave(here("figure/fwd-C.png"))
+
 # performance metrics---------------------------------------------------------------------
+
 perf.metrics <- perf.metrics |>
   pivot_longer(-c(yrs, metric, prod), names_to = "HCR") |>
   mutate(bin = case_when(value >= 0 & value <=.11 ~ "low",
@@ -363,7 +360,7 @@ ggplot(data = filter(perf.metrics, metric == "catch"), aes(x=prod, y = HCR)) +
   scale_fill_viridis_c() +
   facet_grid(.~yrs) +
   labs(x = "productivity", title = "Catch (millions of fish) *bad scales but can fix*")+
-  theme(legend.position = "bottom")+
+  theme(legend.position = "bottom") +
   guides(fill=guide_legend(title="catch (M)"))
 
 my.ggsave(here("figure/perf-catch.png"))
